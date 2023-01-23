fe382780eec06da004dac22c088d646e6e6aa2bd
jobcmax <80607620+jobcmax@users.noreply.github.com>
2023-01-23 22:32:47 +0700

    fix bugs

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 dir=$(pwd)
-cd .local/bin/
-curl https://raw.githubusercontent.com/jobcmax/maxfetch/main/maxfetch -o
+cd ~/.local/bin/
+curl https://raw.githubusercontent.com/jobcmax/maxfetch/main/maxfetch -o maxfetch
 cd $dir
