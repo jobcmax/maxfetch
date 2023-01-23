6fe206c9419b7cbebc46427f8f06d966b7157107
jobcmax <80607620+jobcmax@users.noreply.github.com>
2023-01-23 22:34:15 +0700

    uhhh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 dir=$(pwd)
 cd ~/.local/bin/
-curl https://raw.githubusercontent.com/jobcmax/maxfetch/main/maxfetch -o maxfetch
+curl https://raw.githubusercontent.com/jobcmax/maxfetch/main/maxfetch --output maxfetch
+echo "done"
 cd $dir
