39237c82aad24682b7aab34bfce19e2225585fe7
jobcmax <80607620+jobcmax@users.noreply.github.com>
2023-02-23 19:27:15 +0700

    weird fixes

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 dir=$(pwd)
 cd /usr/local/bin/
 sudo curl -s https://raw.githubusercontent.com/jobcmax/maxfetch/main/maxfetch --output maxfetch
-chmod +x /usr/local/bin/maxfetch
+sudo chmod +x /usr/local/bin/maxfetch
 echo "\033[1;34;48mmaxfetch is installed! \033[1;37;0m" 
 maxfetch
 cd $dir
