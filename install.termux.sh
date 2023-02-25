dc3c1c5f7aee739e81cdea388beba84e2e6bc693
justinlime <justinlime1999@gmail.com>
2023-02-25 11:56:53 -0600

    Added termux support

---
+++ install.termux.sh
@@ -0,0 +1,5 @@
+curl -s https://raw.githubusercontent.com/jobcmax/maxfetch/main/maxfetch.termux > ~/maxfetch 
+chmod +x ~/maxfetch
+mv ~/maxfetch /data/data/com.termux/files/usr/bin
+echo "\033[1;34;48mmaxfetch is installed! \033[1;37;0m" 
+maxfetch
